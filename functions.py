0b73ce553fdfe296cc5bada59e97d0d795a02d78
Shamoon Gill <shamoong469@gmail.com>
2026-06-20 11:41:17 -0700

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,28 @@
+def login(pin):
+   
+    if pin==1122:
+        print("acess granted")
+    else:
+        print("access denied")
+
+    
+    
+user_input=int(input("enter PIN:"))    
+login(user_input)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
